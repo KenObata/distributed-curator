a8eba5c6abdb08a13be766d0e5ed4006e674e91d
KenObata <kenichiderumo@gmail.com>
2026-04-23 18:03:48 -0400

    recognize distributed_curator/__init__.py

--- distributed_curator/__init__.py
+++ distributed_curator/__init__.py
@@ -1,4 +1,5 @@
 from distributed_curator.spark_partition_aware_deduplication import partition_aware_deduplicate
+from distributed_curator.spark_utils import get_jar_path
 
-__all__ = ["partition_aware_deduplicate"]
+__all__ = ["get_jar_path", "partition_aware_deduplicate"]
 __version__ = "0.1.0"
