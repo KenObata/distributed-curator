5d1a1bbd369c23e9f50529709771a5252e168776
Kobata <kenichiderumo@gmail.com>
2026-05-04 17:57:37 -0400

    PyPI doc update (#64)
    * PyPI doc update
    
    * version

--- distributed_curator/__init__.py
+++ distributed_curator/__init__.py
@@ -2,4 +2,4 @@ from distributed_curator.spark_partition_aware_deduplication import partition_aw
 from distributed_curator.spark_utils import get_jar_path
 
 __all__ = ["get_jar_path", "partition_aware_deduplicate"]
-__version__ = "0.1.2"
+__version__ = "0.1.4"
